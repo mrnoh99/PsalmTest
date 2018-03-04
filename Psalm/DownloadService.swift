7caa640f63e2c78a01105e217a7cea4bf8cc01f5
Jaisung Noh <JaisungNOH@air-njs.local>
2018-03-04 09:21:09 +0900

    Initial Commit

---
+++ Psalm/DownloadService.swift
@@ -0,0 +1,59 @@
+
+
+import Foundation
+
+// Downloads song snippets, and stores in local file.
+// Allows cancel, pause, resume download.
+class DownloadService {
+
+  // SearchViewController creates downloadsSession
+  var downloadsSession: URLSession!
+  var activeDownloads: [URL: Download] = [:]
+
+  // MARK: - Download methods called by TrackCell delegate methods
+
+  func startDownload(_ track: Track) {
+    // 1
+    let download = Download(track: track)
+    // 2
+    print (track.previewURL)
+    download.task = downloadsSession.downloadTask(with:  track.previewURL)
+    // 3
+    download.task!.resume()
+    // 4
+    download.isDownloading = true
+    // 5
+    activeDownloads[download.track.previewURL] = download
+  }
+
+  func pauseDownload(_ track: Track) {
+    guard let download = activeDownloads[track.previewURL] else { return }
+    if download.isDownloading {
+      download.task?.cancel(byProducingResumeData: { data in
+        download.resumeData = data
+      })
+      download.isDownloading = false
+    }
+  }
+
+  func cancelDownload(_ track: Track) {
+    if let download = activeDownloads[track.previewURL] {
+      download.task?.cancel()
+      activeDownloads[track.previewURL] = nil
+    }
+  }
+
+  func resumeDownload(_ track: Track) {
+    guard let download = activeDownloads[track.previewURL] else { return }
+    if let resumeData = download.resumeData {
+      download.task = downloadsSession.downloadTask(withResumeData: resumeData)
+    } else {
+      download.task = downloadsSession.downloadTask(with: download.track.previewURL)
+    }
+    download.task!.resume()
+    download.isDownloading = true
+  }
+
+  
+  
+}
---
+++ Psalm/SearchVC+SearchBarDelegate.swift
@@ -0,0 +1,40 @@
+
+
+import Foundation
+import UIKit
+
+extension SearchViewController: UISearchBarDelegate {
+
+//  @objc func dismissKeyboard() {
+//    searchBar.resignFirstResponder()
+//  }
+  
+//  func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+//    dismissKeyboard()
+//      UIApplication.shared.isNetworkActivityIndicatorVisible = true
+//      self.searchResults = queryService.getSearchResults()
+//          self.tableView.reloadData()
+//          self.tableView.setContentOffset(CGPoint.zero, animated: false)
+//
+//  }
+  
+  func listToDownload() {
+    
+   // UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    self.searchResults = queryService.getSearchResults()
+    self.tableView.reloadData()
+    self.tableView.setContentOffset(CGPoint.zero, animated: false)
+  }
+  
+  func position(for bar: UIBarPositioning) -> UIBarPosition {
+    return .topAttached
+  }
+  
+//  func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+//    view.addGestureRecognizer(tapRecognizer)
+//  }
+//  
+//  func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+//    view.removeGestureRecognizer(tapRecognizer)
+//  }
+}
